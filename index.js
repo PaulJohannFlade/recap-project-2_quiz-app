6d6438ce499f017b1f82e855ade3599c25366740
Paul Johann Flade <paul-johann@flade-privat.de>
2024-01-16 10:02:17 +0100

    add toggle functionallity and answer buttns

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // phew… not a lot going on here. Please add some code!
+const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
+
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark--active");
+});
+
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const answer = document.querySelector('[data-js="answer"]');
+
+answerButton.addEventListener("click", () => {
+  answer.classList.toggle("card__answer--active");
+
+  if (answerButton.innerText === "Show answer") {
+    answerButton.innerText = "Hide Answer";
+  } else {
+    answerButton.innerText = "Show answer";
+  }
+});
